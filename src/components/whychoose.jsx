cd594c3934c3d4e816dfc98699328fbf6c48b355
Ronik <102733678+Ronikrr@users.noreply.github.com>
2024-12-18 16:30:45 +0530

    first commet_1

---
+++ src/components/whychoose.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Whychoose = () => {
+    return (
+        <section
+            className="banner relative pt-[100px] lg:py-[100px]  bg-[url('https://uithemez.com/i/artech/home6_modernAgency/assets/img/boxes.png')] bg-cover bg-center">
+            <div className="container mx-auto">
+                <div className="flex flex-col justify-center text-center head">
+                    <h1 className='text-[50px]' >Why Choose Us..!</h1>
+                    <div className="  mx-auto w-[600px]  relative mb-[75px] ">
+                        <div className="absolute bg-black w-[170px] h-[1px] top-[50%]   z-50">
+
+                        </div>
+                        <p className='text-[25px] instrument text-[#ef4523]' >"Why Choose BBD ?"</p>
+                        <div className="absolute bg-black w-[170px] h-[1px] top-[50%] right-0   z-50">
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Whychoose
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from '../components/banner'
 import Header from '../components/header'
+import Whychoose from '../components/whychoose'
+import Aboutsectoion from '../components/aboutsectoion'
 
 const Home = () => {
   return (
-      <div>
-          <Header />
-          <Banner />
+    <div>
+      <Header />
+      <Banner />
+      <Aboutsectoion />
+      <Whychoose />
     </div>
   )
 }
