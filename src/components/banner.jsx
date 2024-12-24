2691fb889f83d445f85e9983e9a375730c8a558a
Ronik <102733678+Ronikrr@users.noreply.github.com>
2024-12-24 16:36:49 +0530

    all

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import boy1 from '../assets/banner/Group_4.png'
 import boy2 from '../assets/banner/Group_5.png'
+import image_1 from '../assets/imag/rb_115959.png'
+import image_2 from '../assets/imag/rb_115959_1.png'
 const Banner = () => {
+
   return (
       <section
           className="banner relative pt-[100px] lg:pt-[200px] min-h-screen bg-[url('https://uithemez.com/i/artech/home6_modernAgency/assets/img/boxes.png')] bg-cover bg-center ">
           {/* <img src="https://uithemez.com/i/artech/home6_modernAgency/assets/img/body_bg.png" className='absolute top-0 left-0 w-full h-full max-h-screen-2xl' alt="" /> */}
-          <div className="absolute bottom-[-190px] right-[-50px]">
-              <img src={boy1} className='w-[70%]' alt="" />
+          <div className="absolute w-[15%] bottom-[-32px] left-[50px]">
+              <img src={boy2} className='w-[70%]' alt="" />
+          </div>
+          <div className="absolute top-[190px] w-[20%]  left-[70px]">
+              <img src={image_1} className='w-full' alt="" />
+              <div className="text-[30px] w-[40%] absolute top-[100px] right-[76px]">
+                  kia kar <br /> rahe ho..
+              </div>
+          </div>
+          <div className="absolute top-[105px] w-[35%]  right-[-50px] ">
+              <img src={image_2} className='w-full' alt="" />
+              <div className="text-[30px] text-center w-[50%] absolute top-[196px] right-[150px]">
+                  kuch nahi <br />
+                  digital agency dundh <br />
+                  raha hu.!
+              </div>
           </div>
           <div className="container px-[15px] mx-auto 2xl:w-[1320px]">
               <div className="relative flex items-center justify-center w-9/12 mx-auto title ">
@@ -28,8 +45,10 @@ const Banner = () => {
           <div className="container px-auto lg:px-0 z-20 relative 2xl:w-[1320px] mx-auto ">
               <h1 className='text-[80px] text-center lg:text-[170px] capitalize ' ><span className='text-[#ef4523]' >delivers</span> results!"</h1>
           </div>
-          <div className="absolute bottom-[70px] left-[50px]">
-              <img src={boy2} className='w-[70%]' alt="" />
+
+          <div className="absolute bottom-[-190px] right-[-50px]">
+              <img src={boy1} className='w-[70%]' alt="" />
+
           </div>
 
       </section>
--- src/components/whychoose.jsx
+++ src/components/whychoose.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Whychoose = () => {
     return (
         <section
-            className="banner relative pt-[100px] lg:py-[100px]  bg-[url('https://uithemez.com/i/artech/home6_modernAgency/assets/img/boxes.png')] bg-cover bg-center">
+            className="banner relative pt-[100px] lg:py-[100px] w-full h-full  ">
+            <div className="absolute w-full h-full">
+                <img src="https://uithemez.com/i/artech/home6_modernAgency/assets/img/boxes.png" className='-rotate-6' alt="" />
+            </div>
             <div className="container mx-auto">
                 <div className="flex flex-col justify-center text-center head">
                     <h1 className='text-[50px]' >Why Choose Us..!</h1>
