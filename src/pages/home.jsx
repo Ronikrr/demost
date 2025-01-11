1221861ce125c084f9c897cd554194ff3dcdbe82
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-11 16:33:20 +0530

    webserver_2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div>
       <Banner />
       <Aboutsectoion />
-      <Whychoose />
+      {/* <Whychoose />  */}
       {/* <HomeVision /> */}
       {/* <Contactbutton /> */}
     </div>
