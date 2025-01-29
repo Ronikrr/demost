531fca1dabb1c9dda4591760a7ee8f326bd5d4f7
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-29 10:13:31 +0530

    ronik10:12

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Banner from '../components/banner'
 import Whychoose from '../components/whychoose'
 import Aboutsectoion from '../components/aboutsectoion'
-import HomeVision from '../components/extra/homevision'
-import Contactbutton from '../components/extra/contactbutton'
+// import HomeVision from '../components/extra/homevision'
+// import Contactbutton from '../components/extra/contactbutton'
 import Exportservice from '../components/exportservice'
 
 const Home = () => {
