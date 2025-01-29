6689d17b330b64c94d3e8a4062e2902490705868
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-29 15:40:20 +0530

    29/1/2025

--- src/components/whychoose.jsx
+++ src/components/whychoose.jsx
@@ -4,8 +4,8 @@ import plus from '../assets/banner/image 178.png';
 
 const Whychoose = () => {
     return (
-        <section className="banner relative  py-[50px] h-full md:h-[200vh] xl:h-[175vh] 2xl:h-[200vh] lg:py-[100px] w-full ">
-            <div className="absolute w-full h-[150vh] -z-10">
+        <section className="banner relative  py-[50px] lg:py-[100px] w-full ">
+            <div className="absolute w-full h-[200vh] -z-10">
                 <img src="https://uithemez.com/i/artech/home6_modernAgency/assets/img/boxes.png" className="w-full h-full -rotate-6" alt="" />
             </div>
             <div className="container mx-auto">
@@ -23,8 +23,8 @@ const Whychoose = () => {
                 <div className="flex flex-col relative  pb-[95px] lg:pb-0 xl:absolute xl:left-32 4xl:top-[200px] 4xl:left-[100px] justify-center w-full mx-auto text-center md:w-[436px] h-full lg:h-[506px] head">
                     <img src={boy_3} className=" xl:w-[300px] xl:h-[365px] 4xl:w-[436px] 4xl:h-[506px]" alt="" />
                 </div>
-                <div className="relative my-10 lg:my-0 flex items-center w-full poppins_thin justify-center lg:pt-[95px]  flex-col sm:flex-row">
-                    <div className="flex flex-col relative right-0 xl:absolute xl:top-[57px] xl:left-[500px] justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
+                <div className="relative my-10 lg:my-0 flex items-center w-full poppins_thin justify-center xl:justify-start lg:pt-[95px]  flex-col sm:flex-row">
+                    <div className="flex flex-col relative right-0  xl:top-[57px] xl:left-[500px] justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
                         <img src={plus} className="hidden lg:block xl:w-[180px] xl:h-[180px]  2xl:left-0 2xl:w-[306px] sm:w-[106px]  2xl:h-[306px] sm:h-[206px] left-[-40%] lg:left-0 absolute" alt="" />
                         <h1 className="text-[18px] sm:text-[24px]">✅ Expert Digital Marketers</h1>
                         <div className="mx-auto w-full md:w-[400px] relative xl:mt-[75px] 2xl:mt-[150px]">
@@ -33,8 +33,8 @@ const Whychoose = () => {
                         <img src={plus} className="hidden lg:block xl:w-[180px] xl:h-[180px]  2xl:right-0 2xl:w-[306px] sm:w-[106px]  2xl:h-[306px] sm:h-[206px] right-[-40%] lg:right-0 absolute" alt="" />
                     </div>
                 </div>
-                <div className="relative my-10 lg:my-0 flex items-center w-full poppins_thin justify-center lg:pt-[95px]  flex-col sm:flex-row">
-                    <div className="flex flex-col relative right-0 xl:absolute xl:top-[457px] xl:right-0 2xl:right-[-3%] justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
+                <div className="relative my-10 lg:my-20 flex items-center  w-full poppins_thin justify-center xl:justify-end lg:pt-[95px]  flex-col sm:flex-row">
+                    <div className="flex flex-col relative right-0  xl:right-0 2xl:right-[-3%] justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
                         <img src={plus} className="hidden lg:block xl:w-[180px] xl:h-[180px]  2xl:left-0 2xl:w-[306px] sm:w-[106px]  2xl:h-[306px] sm:h-[206px] left-[-40%] lg:left-0 absolute" alt="" />
                         <h1 className="text-[18px] sm:text-[24px]">✅ Expert Digital Marketers</h1>
                         <div className="mx-auto w-full md:w-[400px] relative xl:mt-[75px] 2xl:mt-[150px]">
@@ -43,8 +43,8 @@ const Whychoose = () => {
                         <img src={plus} className="hidden lg:block xl:w-[180px] xl:h-[180px]  2xl:right-0 2xl:w-[306px] sm:w-[106px]  2xl:h-[306px] sm:h-[206px] right-[-40%] lg:right-0 absolute" alt="" />
                     </div>
                 </div>
-                <div className="relative my-10 lg:my-0 flex items-center w-full poppins_thin justify-center lg:pt-[95px]  flex-col sm:flex-row">
-                    <div className="flex flex-col relative right-0 xl:absolute xl:top-[525px] xl:left-0 2xl:left-[3%] justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
+                <div className="relative my-10 lg:my-0 flex items-center w-full poppins_thin justify-center xl:justify-start lg:pt-[50px]  flex-col sm:flex-row">
+                    <div className="flex flex-col relative right-0  xl:left-0 2xl:left-[3%] justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
                         <img src={plus} className="hidden lg:block xl:w-[180px] xl:h-[180px]  2xl:left-0 2xl:w-[306px] sm:w-[106px]  2xl:h-[306px] sm:h-[206px] left-[-40%] lg:left-0 absolute" alt="" />
                         <h1 className="text-[18px] sm:text-[24px]">✅ Expert Digital Marketers</h1>
                         <div className="mx-auto w-full md:w-[400px] relative xl:mt-[75px] 2xl:mt-[150px]">
@@ -53,8 +53,8 @@ const Whychoose = () => {
                         <img src={plus} className="hidden lg:block xl:w-[180px] xl:h-[180px]  2xl:right-0 2xl:w-[306px] sm:w-[106px]  2xl:h-[306px] sm:h-[206px] right-[-40%] lg:right-0 absolute" alt="" />
                     </div>
                 </div>
-                <div className="relative my-10 lg:my-0 flex items-center w-full poppins_thin justify-center lg:pt-[95px]  flex-col sm:flex-row">
-                    <div className="flex flex-col relative right-0 xl:absolute xl:top-[800px] xl:left-[500px] justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
+                <div className="relative my-10 lg:my-0 flex items-center w-full poppins_thin justify-center xl:justify-center lg:pt-[200px]  flex-col sm:flex-row">
+                    <div className="flex flex-col relative right-0    justify-center w-full text-center xl:w-[550px] 2xl:w-[754px] head">
                         <img src={plus} className="hidden lg:block xl:w-[180px] xl:h-[180px]  2xl:left-0 2xl:w-[306px] sm:w-[106px]  2xl:h-[306px] sm:h-[206px] left-[-40%] lg:left-0 absolute" alt="" />
                         <h1 className="text-[18px] sm:text-[24px]">✅ Expert Digital Marketers</h1>
                         <div className="mx-auto w-full md:w-[400px] relative xl:mt-[75px] 2xl:mt-[150px]">
