6816ebca354efcf4943c191e75499920df7c8af1
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-02-25 09:31:18 +0530

    index.js

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -14,16 +14,16 @@ const Footer = () => {
                 <div className="pt-[60px] flex flex-wrap border-t w-full border-[#f6f6f6] gap-[30px] ">
                     <div className="lg:p-[10px] mb-[30px] w-full flex flex-col lg:flex-row items-center gap-[30px]">
                         <div className="flex flex-col w-full lg:w-4/12 gap-[30px] ">
-                            <Link className="w-full">
+                            <Link className="w-full cursor-pointer ">
                                 <img src={Logo} className="h-[51px] w-[191px]" alt="" />
                             </Link>
-                            <Link href="" className="w-full flex items-center gap-[15px]">
+                            <Link href="" className="w-full cursor-pointer  flex items-center gap-[15px]">
                                 <div className="w-[30px] h-[30px] border-2 rounded-full border-black flex items-center justify-center">
                                     <FaPhoneAlt />
                                 </div>
                                 <p className="text-[22px] font-medium " >9727168583</p>
                             </Link>
-                            <Link href="" className="w-full flex items-center gap-[15px]">
+                            <Link href="" className="w-full cursor-pointer  flex items-center gap-[15px]">
                                 <div className="w-[30px] h-[30px] border-2 rounded-full border-black flex items-center justify-center">
                                     <BsEnvelope />
                                 </div>
@@ -34,7 +34,7 @@ const Footer = () => {
                             <h4 className="mb-[20px] text-[22px]">Quick Link</h4>
                             <div className="flex flex-col w-full">
                                 {["home", "about", "service", "blog"].map((link) => (
-                                    <Link key={link} to={`/${link}`} className="mb-4  transition-all hover:font-bold  duration-1000 hover:text-[#ef4532] capitalize">
+                                    <Link key={link} to={`/${link}`} className="mb-4 cursor-pointer  transition-all hover:font-bold  duration-1000 hover:text-[#ef4532] capitalize">
                                         {link}
                                     </Link>
                                 ))}
@@ -44,7 +44,7 @@ const Footer = () => {
                             <h4 className="mb-[20px] text-[22px]">support us</h4>
                             <div className="flex flex-col w-full">
                                 {["contact us", "terms & conditions", "career", "help"].map((link) => (
-                                    <Link key={link} to={`/${link.replace(/\s+/g, "-").toLowerCase()}`} className="mb-4 hover:font-bold  transition-all  duration-1000 hover:text-[#ef4532] capitalize">
+                                    <Link key={link} to={`/${link.replace(/\s+/g, "-").toLowerCase()}`} className="mb-4 hover:font-bold  transition-all  duration-1000 hover:text-[#ef4532] capitalize cursor-pointer">
                                         {link}
                                     </Link>
                                 ))}
@@ -54,14 +54,14 @@ const Footer = () => {
                             <h4 className="mb-[20px] text-[15px] lg:text-[22px]">Subscribe Our Newsletter</h4>
                             <div className="flex w-full  items-center gap-3 mb-[20px] flex-row">
                                 <input type="text" className="px-4 py-2 rounded-xl border w-6/12 lg:w-7/12 border-[#ef4523] bg-[#f7f7f7]" />
-                                <button class="bg-[#ef4523] text-[#f7f7f7] border border-[#000000] border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                                <button class="bg-[#ef4523] text-[#f7f7f7] border border-[#000000] border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 cursor-pointer hover:border-b active:opacity-75 outline-none duration-300 group">
                                     <span class="bg-[#000000] shadow-[#000000] absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
                                     subscribe
                                 </button>
                             </div>
                             <div className="flex items-center justify-center gap-4 lg:justify-normal ">
                                 {[FaPinterestP, FaXTwitter, FaFacebookF, FaInstagram].map((Icon, index) => (
-                                    <Link key={index} to="#" className="w-[40px] h-[40px] border-2 transition-all  duration-1000 border-black flex items-center justify-center rounded-full hover:bg-[#ef4523]  ">
+                                    <Link key={index} to="#" className="cursor-pointer w-[40px] h-[40px] border-2 transition-all  duration-1000 border-black flex items-center justify-center rounded-full hover:bg-[#ef4523]  ">
                                         <Icon />
                                     </Link>
                                 ))}
